61f750fc204ce96fcf00305afcc66f23e7143a71
keshavp07-git <keshav.prajapati830@gmail.com>
2024-10-02 12:29:10 +0530

    Working with model and view done

--- src/main/java/com/keshav/JobApp/JobController.java
+++ src/main/java/com/keshav/JobApp/JobController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class JobController {
     @Autowired
@@ -31,5 +33,12 @@ public class JobController {
     public String handleForm(@ModelAttribute JobPost jobPost) {
         service.addJob(jobPost);
         return "success";
+    }
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m){   // Using Model and making of its object m to work with dummy data.
+        List<JobPost> jobs = service.getAllJob(); // In a List named jobs calling all jobs to view using getAllJob() with object of service this service referring to JobService class
+        m.addAttribute("jobPosts",jobs); // To see all Data from model which make using List we use Model create its object m
+        //with attributeName "jobPosts" we access jobs which is working as a variable to hold all job details
+        return "viewalljobs";
     }
 }
